4d0d48f839f965e1cd29bb8f91de84142952876e
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-06-10 10:00:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,30 @@
+// Copyright 2021 NNTU-CS
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-    BST<std::string> tree;
-
     std::ifstream file(filename);
-    if (!file) {
+    BST<std::string> tree;
+    std::string text;
+    if (file) {
+        while (!file.eof()) {
+            text.erase();
+            while (true) {
+                char chr = file.get();
+                if (chr >= 'A' && chr < 'Z'
+                   || chr >= 'a' && chr <= 'z')
+                    text += tolower(chr);
+                else
+                    break;
+                }
+            if (!text.empty())
+                tree.newNode(text);
+        }
+        file.close();
+    } else {
         std::cout << "File error!" << std::endl;
     }
-
-    std::string countwords;
-
-    while (!file.eof()) {
-        char ch = tolower(file.get());
-        if (isalpha(ch)) {
-            countwords = countwords + ch;
-        } else {
-            tree.add(countwords);
-            countwords.clear();
-        }
-    }
-    file.close();
     return tree;
 }
