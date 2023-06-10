534ae647817bbf212164a5e38905f8733a71d72b
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-06-10 09:49:36 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,65 +1,64 @@
-// Авторское право 2021 NNTU-CS
-#ifndef ВКЛЮЧАЕТ В СЕБЯ_BST_H_
-#определить INCLUDE_BST_H_
-#включить <алгоритм>
-
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-#include <algorithm>
-template<typename T>
+#include <iostream>
+#include <fstream>
+
+template <typename T>
 class BST {
  private:
- struct Node {
-  T value;
-  int count;
-  Node *left;
-  Node *right;
- };
- Node* root;
- Node* addNode(Node *root, const T& value) {
-  if (root == nullptr) {
-   root = new Node;
-   root->value = value;
-   root->count = 1;
-   root->left = root->right = nullptr;
-  } else if (root->value > value) {
-   root->left = addNode(root->left, value);
-  } else if (root->value < value) {
-   root->right = addNode(root->right, value);
-  } else {
-   root->count++;
-  }
-  return root;
- }
- Node* searchNode(Node* root, const T& value) {
-  if (root == nullptr || root->value == value) {
-   return root;
-  }
-  if (root->value > value) {
-   return searchNode(root->left, value);
-  }
-  return searchNode(root->right, value);
- }
- int depthTree(Node* root) {
-  if (root == nullptr) return 0;
-  return std::max(depthTree(root->left), depthTree(root->right)) + 1;
- }
+    struct Node {
+        T val;
+        unsigned int count;
+        Node* l;
+        Node* r;
+    };
+    Node* root = nullptr;
+    int searchNodeTree(Node* root, const T& value) {
+        if (root == nullptr) {
+            return 0;
+        } else if (root->val > value) {
+            return searchNodeTree(root->l, value);
+        } else if (root->val < value) {
+            return searchNodeTree(root->r, value);
+        } else {
+            return root->count;
+        }
+        return 0;
+    }
+    int getDepthTree(Node* root) {
+        if (root == nullptr)
+            return 0;
+        int rDepth = getDepthTree(root->r),
+            lDepth = getDepthTree(root->l);
+        return (lDepth > rDepth) ? lDepth + 1 : rDepth + 1;
+    }
+    Node* newNodeTree(Node* root, const T& value) {
+        if (root == nullptr) {
+            root = new Node;
+            root->val = value;
+            root->count = 1;
+            root->l = root->r = nullptr;
+        } else if (root->val < value) {
+            root->r = newNodeTree(root->r, value);
+        } else if (root->val > value) {
+            root->l = newNodeTree(root->l, value);
+        } else {
+            root->count+=1;
+        }
+        return root;
+    }
+
  public:
- BST() : root(nullptr) {}
- int depth() {
-  return depthTree(root) - 1;
- }
- int search(const T& value) {
-  Node* i = searchNode(root, value);
-  if (i != nullptr) {
-   return i->count;
-  } else {
-   return  0;
-  }
- }
- void add(const T& value) {
-  root = addNode(root, value);
- }
+    int search(const T& value) {
+        return searchNodeTree(root, value);
+    }
+    int depth() {
+        return getDepthTree(root) - 1;
+    }
+    void newNode(const T& value) {
+        root = newNodeTree(root, value);
+    }
 };
-#endif  //
+
+#endif  // INCLUDE_BST_H_
